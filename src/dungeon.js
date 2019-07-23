261a99ed4ee764765939e5e41b6d5a76c2da8318
jonneale@gmail.com <jonneale@gmail.com>
2019-07-23 09:23:18 +0100

    Consolidated dungeon generation

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,3 +1,4 @@
+"use strict"
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 const survivalThreshold = 5;
@@ -5,78 +6,82 @@ const birthThreshold = 6;
 const initializationChance = 0.45;
 const PIXEL_WIDTH = 5;
 const PIXEL_HEIGHT = 5;
+const WIDTH = 100;
+const HEIGHT = 100;
+const NEIGHBOUR_COORDS = [[-1,-1],[-1,0],[-1,1],[0,0],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 
-function neighbourCount (map, x, y){
-	var aliveNeighbours = 0;
-	for(xOffset = -1; xOffset < 2; xOffset++){
-		for(yOffset=-1; yOffset < 2; yOffset++){
-			dx = x+xOffset;
-			dy = y+yOffset;
-			if((dx > 0) && (dx < map.length) && (dy > 0) && (dy < map[dx].length)){
-				aliveNeighbours += map[dx][dy];
-			}
-			else {
-				aliveNeighbours++;
-			}
-		}
-	}
-	return aliveNeighbours;			
+var grid = [];
+
+function initialiseGrid() {
+    for (var i = 0; i < WIDTH*HEIGHT; i++){
+        grid[i] = Math.random() > initializationChance ? 1 : 0;
+    }
 }
 
-
-function initialiseMap() {
-	var width = 100;
-	var height = 100;
-	var map = [];
-	for (i = 0; i < height; i++){
-		map[i] = [];
-		for (j = 0; j < width; j++){
-			map[i][j]= Math.random() > initializationChance ? 1 : 0;
-		}
-	}
-	return map;
-}
-
-function drawDungeon(map){
-	for (i = 0; i < map.length; i++){
-		for(j = 0; j < map[i].length; j++){
-			context.fillStyle = map[i][j] == 1 ? "grey" : "white";
-			context.fillRect(j*PIXEL_WIDTH,(i*PIXEL_HEIGHT),PIXEL_WIDTH,PIXEL_HEIGHT);				
-		}
+function drawGrid(){
+	for (var index = 0; index < WIDTH*HEIGHT; index++) {
+		var valueOfCell = grid[index];
+		context.fillStyle = valueOfCell == 1 ? "grey" : "white";
+		var yValue = (index/WIDTH) | 0;
+		var xValue = index % WIDTH;
+		context.fillRect(xValue*PIXEL_WIDTH,(yValue*PIXEL_HEIGHT),PIXEL_WIDTH,PIXEL_HEIGHT);
 	}
 }
 
-function updateMap (map){
-	var updatedMap = [];
-	for(i=0; i < map.length; i++){
-		updatedMap[i] = []
-		for(j = 0; j < map[i].length; j++){
-			var cell = map[i][j];
-			var aliveNeighbours = neighbourCount(map,i,j);
 
-			if (cell == 1){
-				updatedMap[i][j]= aliveNeighbours >= survivalThreshold ? 1 : 0;
-			}
-			else {
-				updatedMap[i][j]= aliveNeighbours >= birthThreshold ? 1 : 0;
-			}
-			
-		}
-	}
-	return updatedMap;
+function countNeighbours(cell, grid){
+    return NEIGHBOUR_COORDS.reduce(function(accumulator,v){
+        var coord = cell + v[0] + (WIDTH * v[1]);
+        if (coord < 0 || coord > WIDTH*HEIGHT) {
+          return accumulator + 1;
+        } else if ((cell % WIDTH == 0) && v[0] < 0) {
+            return accumulator + 1;
+        } else if (((cell+1) % WIDTH == 0) && v[0] > 0) {
+            return accumulator + 1;
+        } else {
+            return accumulator + grid[coord];
+        }   
+    },0)
 }
 
-
-function mainLoop(map,iteration){
-	console.log(iteration);
-	drawDungeon(map);
-	updatedMap = updateMap(map);
-	if (iteration < 5){
-		setTimeout(mainLoop ,2000, updatedMap,iteration+1)	
-	}
+function updateGrid(){
+    var oldGrid = [...grid];      
+    for(var i=0; i < WIDTH*HEIGHT; i++){
+        var cell = grid[i];
+        var aliveNeighbours = countNeighbours(i,oldGrid);
+        if (cell == 1){
+            grid[i] = aliveNeighbours >= survivalThreshold ? 1 : 0;
+        }
+        else {
+            grid[i] = aliveNeighbours >= birthThreshold ? 1 : 0;
+        }
+    }
 }
 
-	
-function  run(){
-	mainLoop(initialiseMap(),0)	
+function mainLoop(iteration){
+    updateGrid();
+    drawGrid();
+    if (iteration < 5){
+        setTimeout(mainLoop ,2000,iteration+1);
+    }
 }
+
+function toggleCell(event){
+    var x = event.pageX - canvas.offsetLeft,
+        y = event.pageY - canvas.offsetTop;
+    var xValue = x / PIXEL_WIDTH | 0,
+        yValue = y / PIXEL_HEIGHT | 0;
+    var coord = yValue * HEIGHT + xValue;
+    grid[coord] = 1 - grid[coord];
+    drawGrid();
+}
+
+canvas.addEventListener('click', toggleCell, false);
+
+
+function run(){
+    mainLoop(0);
+}
+
+initialiseGrid();
+drawGrid();
